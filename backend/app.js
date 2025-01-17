ffc6dd867969ef15bd57ca9fde52dec9760253de
YawObengAsante <yawobengasante@gmail.com>
2025-01-17 14:42:51 -0800

    project completed

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./db/connectDB.js";
 import ProductRoutes from "./routes/product.js";
 
@@ -7,10 +8,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/v1/products", ProductRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 const start = async () => {
   try {
