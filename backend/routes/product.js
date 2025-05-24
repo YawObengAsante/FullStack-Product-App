9e8147fc32fef00a33442744057c0bea6d9364a4
Yaw Obeng Asante <yawobengasante@gmail.com>
2025-05-24 20:00:00 +0000

    chore: changed router to @minisylar/express-typed-router

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,30 @@
-import express from 'express'
-import {createProduct, deleteProduct, getAllProducts, updateProduct} from '../controllers/product.js'
+import express, { json } from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  updateProduct,
+} from "../controllers/product.js";
+import { createTypedRouter } from "@minisylar/express-typed-router";
+// import Product from "../models/product.model.js";
 
-const router = express.Router()
+// const router = express.Router()
+const router = createTypedRouter();
 
-router.route('/').get(getAllProducts).post(createProduct)
-router.route('/:id').put(updateProduct).delete(deleteProduct)
+router.get("/", getAllProducts).post("/", createProduct)
+router.put('/:id',updateProduct).delete('/:id', deleteProduct)
 
-export default router
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const products = await Product.find({});
+//     res.status(200).json({ success: true, data: products });
+//   } catch (error) {
+//     console.error("Error in fetching Products: ", error.message);
+//     res.status(500).json({ success: false, message: "Server Error" });
+//   }
+// });
+
+const expressRouter = router.getRouter()
+
+export default expressRouter;
